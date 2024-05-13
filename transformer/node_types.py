d99231c393a1cfab0622a4de8bbfe39e262b78e0
Joon Lee <joon.lee2@nih.gov>
2024-05-13 14:32:45 -0400

    Moved some constants for the transformation script to their own files to be imported

---
+++ transformer/node_types.py
@@ -0,0 +1,7 @@
+# Enum of node types in the datamodel
+class NODE_TYPES(Enum):
+    DIAGNOSIS = "Diagnosis"
+    PARTICIPANT = "Participant"
+    REFERENCE_FILE = "Reference File"
+    STUDY = "Study"
+    SURVIVAL = "Survival"
